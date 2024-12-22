4ab56d1f08494f30b9dc10e18a62396c1b954e2d
agarnung <garnungalejandro@gmail.com>
2024-12-22 17:57:56 +0100

    Expanding taxonomy and nested/carets logic

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,13 @@
 // When clicking caret, set nested items opened
 document.querySelectorAll('.caret').forEach(caret => {
-    caret.addEventListener('click', () => {
-      const nested = caret.nextElementSibling;
-      if (nested) {
-        nested.style.display = nested.style.display === 'block' ? 'none' : 'block';
-      }
-    });
+  caret.addEventListener('click', () => {
+    // Toggle the display of nested items
+    const nested = caret.nextElementSibling;
+    if (nested) {
+      nested.style.display = nested.style.display === 'block' ? 'none' : 'block';
+    }
+
+    // Toggle the active state of the caret to change the icon
+    caret.classList.toggle('active');
   });
-  
+});
